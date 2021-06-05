4a988075866c76d91becf7a2e99a55f38fcdc9b4
E. Almqvist <elalmqvist@gmail.com>
2021-06-05 14:04:51 +0200

    Colors update

--- config.h
+++ config.h
@@ -8,18 +8,15 @@ static const char *fonts[] = {
 };
 
 static const char bg_color[]		= "#111015";
-static const char col_gray1[]		= "#222222";
-static const char col_gray2[]		= "#444444";
-static const char col_gray3[]		= "#bbbbbb";
-static const char col_gray4[]		= "#eeeeee";
+static const char col_foreg[]		= "#fefefe";
 static const char col_cyan[]		= "#5f819d";
 
 static const char *prompt = NULL;	  /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*					fg			 bg	   */
-	[SchemeNorm] =	{ col_gray3, bg_color },
-	[SchemeSel] =	{ col_gray4, col_cyan },
-	[SchemeOut] =	{ col_gray4, col_cyan },
+	[SchemeNorm] =	{ col_foreg, bg_color },
+	[SchemeSel] =	{ col_foreg, col_cyan },
+	[SchemeOut] =	{ col_foreg, col_cyan },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 16;
