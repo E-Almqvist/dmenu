2ca3a4d32c05cbee5a943b7d6477b20a5a3453cc
E. Almqvist <elalmqvist@gmail.com>
2021-07-26 22:33:50 +0200

    Font update

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;					  /* -b  option; if 0, dmenu appears at bottom	 */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Code:size=11"
+	"Iosevka:size=12"
 };
 
 static const char bg_color[]		= "#111015";
